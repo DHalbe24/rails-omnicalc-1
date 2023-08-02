ea1ec886c67a1b683c85d90ed8a91a8b0102884b
DHalbe24 <137205368+DHalbe24@users.noreply.github.com>
2023-08-02 20:05:36 +0000

    squares and roots are working

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -1,10 +1,26 @@
 class CalcController < ApplicationController
   def square
-    @number = params.fetch("number")
+    render({ :template => "calc_templates/square_form"})
+  end
 
-    @square_results = @number ** 2.0
+  def square_results
+    @number = params.fetch("number").to_f
 
-    render({ :template => "calc_templates/square"})
+    @result = @number ** 2.0
+
+    render({ :template => "calc_templates/square_results"})
+  end
+
+  def square_root
+    render({ :template => "calc_templates/square_root_form"})
+  end
+
+  def square_root_results
+    @number = params.fetch("number").to_f
+
+    @result = @number ** (1.0/2.0)
+
+    render({ :template => "calc_templates/square_root_results"})
   end
 end  
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get("/square", { :controller => "calc", :action => "square" })
+  get("/square/new", { :controller => "calc", :action => "square" })
 
-  #get("/square_root", { :controller => "calc", :action => "square_root" })
+  get("/square/results", { :controller => "calc", :action => "square_results" })
+
+  get("/square_root/new", { :controller => "calc", :action => "square_root" })
+
+  get("/square_root/results", { :controller => "calc", :action => "square_root_results" })
 
   #get("/payment", { :controller => "calc", :action => "payment" })
 end
